d19b377ac7b0b95d82215854106042af7d601d31
Kepilyn V <80963070+NylipekWolf@users.noreply.github.com>
2021-07-30 13:18:37 -0300

    uma pequena pausa

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
+const gestPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
+
 const fetchpokemon = () => {
-    const gestPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
+
+    const generatePokemonPromise = () => {}
+    const pokemonPromises = []
+
     for (let i = 1; i <= 150; i++) {
-        fetch(url).then(response => response.json())
+        pokemonPromises.push(fetch(gestPokemonUrl(i)).then(response => response.json()))
     }
 
-    fetch(gestPokemonUrl)
-        .then(response => response.json())
-        .then( pokemon => {
-            console.log(pokemon)
+    Promise.all(pokemonPromises)
+        .then(pokemons => {
+
+            const listPokemons = pokemons.reduce((accumlator, pokemon) => {
+                const types = pokemon.types.map(typeInfo => typeInfo.type.name)
+                accumlator += `
+                    <li class="card ${types[0]}">
+                        <img class="card-image " alt="${pokemon.name}" src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png" />
+                        <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
+                        <p class="card-subtitle">${types.join(" | ")}</p>
+                    </li>
+                `
+                return accumlator
+            }, '')
+
+            const ul = document.querySelector('[data-js="pokedex"]') 
+
+            ul.innerHTML = listPokemons
         })
 }
 fetchpokemon()
